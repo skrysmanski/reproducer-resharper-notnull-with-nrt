09e223d321b6ce53e8fd90715ea92bea0ed4112e
Sebastian Krysmanski <noreply@manski.net>
2020-10-01 09:17:17 +0200

    Added reproducer

--- ReSharperReproducer/Reproducer.cs
+++ ReSharperReproducer/Reproducer.cs
@@ -1,8 +1,26 @@
-﻿namespace ReSharperReproducer
+﻿using JetBrains.Annotations;
+
+namespace ReSharperReproducer
 {
     public class Reproducer
     {
-        public void ReproducerMethod()
+#nullable enable
+
+        public void Broken(object? obj)
+        {
+            DoSomething(obj); // <-- should produce a "Possible 'null' assignment to non-nullable entity" warning
+        }
+
+#nullable disable
+
+        public void Works([CanBeNull] object obj)
+        {
+            DoSomething(obj); // <-- does produce this warning
+        }
+
+#nullable restore
+
+        public void DoSomething<T>([NotNull] T input)
         {
 
         }
